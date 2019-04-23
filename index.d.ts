9f473f571f9464f1f74ac16109c7e06ee8cf8bc0
Zhigang Fang <zhigang1992@gmail.com>
2019-04-23 11:17:18 +0800

    chore: add typescript definition

---
+++ index.d.ts
@@ -0,0 +1,34 @@
+declare module 'react-native-popover-view' {
+  import {Component, Ref} from 'react';
+  import {Animated, StyleProp, View, ViewStyle} from 'react-native';
+
+  export class Rect {
+    constructor(x: number, y: number, width: number, height: number);
+  }
+
+  export class Size extends Rect {
+    constructor(width: number, height: number)
+  }
+
+  interface PopoverViewProps {
+    isVisible: boolean;
+    mode?: 'rn-modal' | 'js-modal' | 'tooltip';
+    fromView?: Ref<View>;
+    fromRect?: Rect;
+    fromDynamicRect?: (displayAreaWidth: number, displayAreaHeight) => Rect;
+    displayArea?: Rect;
+    placement?: 'top' | 'bottom' | 'left' | 'right' | 'auto';
+    animationConfig?: Animated.TimingAnimationConfig;
+    verticalOffset?: number;
+    popoverStyle?: StyleProp<ViewStyle>;
+    arrowStyle?: StyleProp<ViewStyle>;
+    backgroundStyle?: StyleProp<ViewStyle>;
+    onOpenStart?: () => void;
+    onOpenComplete?: () => void;
+    onRequestClose?: () => void;
+    onCloseStart?: () => void;
+    onCloseComplete?: () => void;
+    debug?: boolean;
+  }
+  export default class Popover extends Component<PopoverViewProps> {}
+}
