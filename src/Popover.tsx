1d273d3eeba844caf3592220d4b4f513c1c75755
SteffeyDev <steffeydev@icloud.com>
2021-04-14 20:53:08 -0400

    Addressing issue from #100, tooltip not showing properly

--- src/Popover.tsx
+++ src/Popover.tsx
@@ -325,7 +325,7 @@ class JSModalPopover extends Component<JSModalPopoverProps, ModalPopoverState> {
       return (
         <View
           pointerEvents="box-none"
-          style={[styles.container, { left: 0 }]}
+          style={styles.container}
           ref={this.containerRef}>
           <AdaptivePopover
             onCloseComplete={() => {
@@ -1154,7 +1154,7 @@ class BasePopover extends Component<BasePopoverProps, BasePopoverState> {
     }
 
     return (
-      <View pointerEvents="box-none" style={[styles.container, { left: 0 }]}>
+      <View pointerEvents="box-none" style={[styles.container, { top: -1 * FIX_SHIFT }]}>
         <SafeAreaView
           pointerEvents="none"
           forceInset={this.props.safeAreaInsets}
@@ -1194,7 +1194,7 @@ class BasePopover extends Component<BasePopoverProps, BasePopoverState> {
 
 const styles = StyleSheet.create({
   container: {
-    top: -1 * FIX_SHIFT,
+    top: 0,
     bottom: 0,
     left: 0,
     right: 0,
